9b8c89cd2a9ef4d5e4ed08f3f0ff62f6b15f871f
tantd9 <tranduytan.220300@gmail.com>
2023-10-18 17:03:15 +0700

    fail section 65

--- ecommerce/account/urls.py
+++ ecommerce/account/urls.py
@@ -4,13 +4,12 @@ from .import  views
 
 urlpatterns = [
      
- path ('register', views.register , name= 'register'),
-
+    path ('register', views.register , name= 'register'),
 
 
      # Email verification URL's
-
-    # path('email-verification', views.email_verification, name='email-verification'),
+     
+    path('email-verification/<str:uidb64>/<str:token>/', views.email_verification, name='email-verification'),
 
 
     path('email-verification-sent', views.email_verification_sent, name='email-verification-sent'),
--- ecommerce/account/views.py
+++ ecommerce/account/views.py
@@ -1,7 +1,19 @@
 from django.shortcuts import render, redirect
 
+from django.contrib.auth.models import User
 
 from .forms import CreateUseForm
+
+from django.contrib.sites.shortcuts import get_current_site
+
+from . token import user_tokenizer_generate
+
+from django.template.loader import render_to_string
+
+from django.utils.encoding import force_bytes,force_str
+
+from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
+
 
 
 
@@ -14,10 +26,34 @@ def register(request):
           form = CreateUseForm(request.POST)
 
           if form.is_valid():
+              
+              user = form.save()
 
-               form.save()
+              user.is_active = False
 
-               return redirect('store')
+              user.save()
+
+            
+              # Email verification setup (template)
+
+              current_site = get_current_site(request)
+
+              subject = 'Account verification email'
+
+              message = render_to_string('account/registration/email-verification.html', {
+            
+                'user': user,
+                'domain': current_site.domain,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': user_tokenizer_generate.make_token(user),
+            
+            })
+
+              user.email_user(subject=subject, message=message)
+
+
+              return redirect('email-verification-sent')
+
 
      context = {'form': form}
  
@@ -28,6 +64,31 @@ def register(request):
 # def email_verification(request, uidb64, token):
 
 
+def email_verification(request, uidb64, token):
+
+    # uniqueid
+
+    unique_id = force_str(urlsafe_base64_decode(uidb64))
+
+    user = User.objects.get(pk=unique_id)
+    
+    # Success
+
+    if user and user_tokenizer_generate.check_token(user, token):
+
+        user.is_active = True
+
+        user.save()
+
+        return redirect('email-verification-success')
+
+
+    # Failed 
+
+    else:
+
+        return redirect('email-verification-failed')
+        
 
 def email_verification_sent(request):
 
