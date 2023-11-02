d2b9c2d254210298282dba04b17543f04114a499
tantd9 <tranduytan.220300@gmail.com>
2023-11-02 15:03:19 +0700

    buil address model

--- ecommerce/payment/admin.py
+++ ecommerce/payment/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
+
+from . models import ShippingAddress
+
+admin.site.register(ShippingAddress)
 # Register your models here.
--- ecommerce/payment/models.py
+++ ecommerce/payment/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+from django.contrib.auth.models import User
+
+
+class ShippingAddress (models.Model):
+
+    full_name = models.CharField(max_length=300)
+
+    email = models.CharField(max_length=250)
+
+    address  = models.CharField (max_length=300)
+
+    city =  models.CharField(max_length=250)
+
+    #FK
+    # Authenticated / not authenticated user (bear in mind)
+    user = models.ForeignKey(User, on_delete=models.Case,null=True,blank=True)
+
+    class Meta:
+        
+        verbose_name_plural = 'Shipping Address'
+
+    
+    def __str__(self) :
+        
+        return 'Shipping Address - ' +str(self.id)
